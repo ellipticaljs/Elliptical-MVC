37843dffc55f2109fb6ebc59d6ac587bfabc586e
Scott <sfrancis@misinteractive.com>
2016-05-11 14:02:12 -0400

    Updated HtmlForm

---
+++ Elliptical/Extensions/Json.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RevStack.Mvc;
+
+namespace Elliptical.Mvc
+{
+    static partial class Extensions
+    {
+        public static string toJsonString<T>(this T src)
+        {
+            return Json.SerializeObject<T>(src, true, true);
+        }
+    }
+}
---
+++ Elliptical/UI/HtmlForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Elliptical.Mvc
+{
+    public class HtmlForm
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Method { get; set; }
+        public string Action { get; set; }
+        public string SuccessMessage { get; set; }
+        public string FailureMessage { get; set; }
+        public Dictionary<string,string> ReadOnly { get; set; }
+        public Dictionary<string,string> Required { get; set; }
+        public string Service { get; set; }
+        public string NotifyService { get; set; }
+        public string ValidationService { get; set; }
+        public string ButtonLabel { get; set; }
+
+        public HtmlForm()
+        {
+            ReadOnly = new Dictionary<string, string>();
+            Required = new Dictionary<string, string>();
+        }
+    }
+}
