3f92944ba5cf9e23b277f27e297327073c719641
Scott <sfrancis@misinteractive.com>
2016-06-24 02:40:21 -0400

    Added MetaOpenGraph

---
+++ Elliptical/HtmlHelpers/OpenGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Elliptical.Mvc
+{
+    public partial class HtmlHelpers
+    {
+        public IHtmlString MetaOpenGraph(Dictionary<string,string> graph)
+        {
+            if (graph == null) return null;
+
+            string meta = "";
+            foreach(var item in graph)
+            {
+                meta += "<meta property=\"og:" + item.Key + "\" content=\"" + item.Value + "\" />" + Environment.NewLine;
+            }
+            return new HtmlString(meta);
+        }
+    }
+}
